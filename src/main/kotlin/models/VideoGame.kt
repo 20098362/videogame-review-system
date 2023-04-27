114b14dbfb646050f8764c2005529e730d06c280
Troy <20098362@mail.wit.ie>
2023-04-27 15:34:59 +0100

    Update Company model to use VideoGame model

--- src/main/kotlin/models/VideoGame.kt
+++ src/main/kotlin/models/VideoGame.kt
@@ -1,9 +1,11 @@
 package models
 
 data class VideoGame(
+    var gameId: Int,
     var title: String,
     var platform: String,
     var genre: String,
     var budget: Int,
     var profit: Int
-)
+){
+}

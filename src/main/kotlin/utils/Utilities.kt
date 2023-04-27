445ef6c4d99c0276b46e7f42120f0acd1f06b1b8
Troy <20098362@mail.wit.ie>
2023-04-27 13:58:23 +0100

    Update Utilities methods

--- src/main/kotlin/utils/Utilities.kt
+++ src/main/kotlin/utils/Utilities.kt
@@ -1,6 +1,18 @@
 package utils
 
+import models.Company
+
 object Utilities {
     @JvmStatic
     fun isValidListIndex(index: Int, list: List<Any>): Boolean = (index >= 0 && index < list.size)
+
+    @JvmStatic
+    fun formatListString(companiesToFormat: List<Company>): String =
+        companiesToFormat
+            .joinToString(separator = "\n") { company ->  "$company" }
+
+    @JvmStatic
+    fun formatSetString(videoGamesToFormat: Set<VideoGame>): String =
+        videoGamesToFormat
+            .joinToString(separator = "\n") { videoGame ->  "\t$videoGame" }
 }
